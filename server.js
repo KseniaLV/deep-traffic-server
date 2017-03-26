a888ab232f856a775e4e17ef0b7a3e7a87e1a44f
KseniaLV <lagutinakv@mail.ru>
2017-03-26 18:48:09 +0300

    add openshift nodejs settings

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = require('bluebird');
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 var app = express();
 app.use(bodyParser.json());
 
@@ -82,5 +85,5 @@ app.post('/api/newUser', function(request, response){
     response.send('Data is saved');
 });
 
-app.listen('4500');
+app.listen(server_port, server_ip_address);
 
