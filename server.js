3fb9af9ff3658771895870468aaacbf4969e1e6d
Ksenia Lagutina <lagutinakv@mail.ru>
2017-04-07 18:11:40 +0300

    Add sending email

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-var express = require('express'), bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+let express = require('express'), bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let nodemailer = require('nodemailer');
 
 mongoose.Promise = require('bluebird');
 
-var app = express();
+let app = express();
 app.use(bodyParser.json());
 
-var port = process.env.port || 4500;
+let port = process.env.port || 4500;
 
 mongoose.connect('mongodb://ksenialv:1007905k!@clusterdeeptr-shard-00-00-22vim.mongodb.net:27017,clusterdeeptr-shard-00-01-22vim.mongodb.net:27017,clusterdeeptr-shard-00-02-22vim.mongodb.net:27017/deepTrafficData?ssl=true&replicaSet=ClusterDeepTr-shard-0&authSource=admin');
+ 
+/*app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '');
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});*/
 
-var userSchema = new mongoose.Schema({
-       // _id: 'string',
+let userSchema = new mongoose.Schema({
 	user: 'string',
 	mph: 'number'
 });
@@ -20,25 +27,41 @@ userSchema.statics.findByName = function (name, cb) {
   return this.find({ user: new RegExp(name, 'i') }, cb);
 }
 
-var UserModel = mongoose.model('users', userSchema);
+let UserModel = mongoose.model('users', userSchema);
+
+let transporter = nodemailer.createTransport({
+    service: 'Yandex',
+    auth: {
+        user: 'deepTraffic@yandex.ru',
+        pass: 'traffic704!'
+    }
+});
+
+let mailOptions = {
+    from: '"Deep Traffic" <deepTraffic@yandex.ru>',
+    to: 'user',
+    subject: 'Deep Traffic competition',
+    text: 'Your last result is '
+};
 
 app.get('/api/',function(req,res) {
 	res.send('Working');
 });
 
-app.get('/api/userrecords', function(req,res) {
+app.get('/api/users', function(req,res) {
 	UserModel.find({}).sort({mph: -1}).limit(10).exec(function(err,docs) {
 		if(err) {
 			res.send(err);
 		}
 		else {
-			res.send({userrecords: docs});
+			res.send({data: docs});
 		}
 	});
 });
 
-app.post('/api/newUser', function(request, response){
-    var newUser = new UserModel(request.body);
+app.post('/api/newUsers', function(request, response){
+    let newUser = new UserModel(request.body);
+    let bestResult = newUser.mph, currentResult = newUser.mph;
     UserModel.findByName(newUser.user, function (err, similarUsers) {
         if (similarUsers.length === 0) {
             newUser.save(function (err) {
@@ -49,7 +72,7 @@ app.post('/api/newUser', function(request, response){
             });
         }
         else {
-            var user = similarUsers[0];
+            let user = similarUsers[0];
             if (newUser.mph > user.mph) {
                 user.mph = newUser.mph;
                 user.save(function (err) {
@@ -58,10 +81,19 @@ app.post('/api/newUser', function(request, response){
                         return err;
                     }
                 });
+            } else {
+                bestResult = user.mph;
             }
         }
+        mailOptions.to = request.body.email;
+        mailOptions.text = 'Your last result is ' + currentResult + " kmph. Your best result is " + bestResult + " kmph.";
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+        });
     });
     response.send('Data is saved');
 });
 
-var server = app.listen(port);
+let server = app.listen(port);
